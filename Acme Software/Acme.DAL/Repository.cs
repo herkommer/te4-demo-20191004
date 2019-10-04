e35f335d38d02a4958ee189ac5e224cba323b5ef
herkommer <m@herkommer.co.uk>
2019-10-04 10:18:41 +0200

    testing adding DB support for repository

--- Acme Software/Acme.DAL/Repository.cs
+++ Acme Software/Acme.DAL/Repository.cs
@@ -4,12 +4,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Acme.DAL
 {
     public class Repository
     {
         private List<Customer> _customers = new List<Customer>();
+
+        //Hmm, jag vill testa att använda en DB istället för List<T>
+        //jag behöver kontakt med DB
+        //det borde finnas hjälp att få i Framework
+
+        public Repository()
+        {
+            //constructor, dvs denna kod körs vid NEW
+            SqlConnection cn = new SqlConnection(@"Data Source=(localdb)\mssqllocaldb;Initial Catalog=ACME;Integrated Security=True");
+            cn.Open();
+
+            SqlCommand cm = new SqlCommand("INSERT INTO Customers (Name) VALUES ('Mimmi Pigg');");
+            cm.Connection = cn;
+            cm.ExecuteNonQuery();
+
+            cn.Close();
+        }
+
         public void Save(Customer customer)
         {
             _customers.Add(customer);
